ec938f6b7f863e24672a93995ec03606087ea665
Monet <monet@Monets-MacBook-Pro-2.local>
2019-04-30 15:37:35 +0530

    Initial commit

---
+++ MySdkMonet/Launcher.swift
@@ -0,0 +1,34 @@
+//
+//  Launcher.swift
+//  MySdkMonet
+//
+//  Created by Monet  on 30/04/19.
+//  Copyright © 2019 MonetNetworks. All rights reserved.
+//
+
+import UIKit
+import HaishinKit
+
+public class Launcher: UIViewController {
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func lauch (){
+        print("sdk launche")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MySdkMonet/MySdkMonet.h
@@ -0,0 +1,19 @@
+//
+//  MySdkMonet.h
+//  MySdkMonet
+//
+//  Created by Monet  on 30/04/19.
+//  Copyright © 2019 MonetNetworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MySdkMonet.
+FOUNDATION_EXPORT double MySdkMonetVersionNumber;
+
+//! Project version string for MySdkMonet.
+FOUNDATION_EXPORT const unsigned char MySdkMonetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySdkMonet/PublicHeader.h>
+
+
